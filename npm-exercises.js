4ca984c7247fdb6fa1102f88d60e2877210af6aa
gmartinez31 <bhgustavo13@yahoo.com>
2017-10-27 15:31:35 -0500

    finished npm and module exercises

---
+++ npm-exercises.js
@@ -0,0 +1,6 @@
+var marked = require('marked');
+console.log(marked('Using dis bish Mark __markdown__'));
+
+var lodash = require('lodash');
+arr = [2, 234, 345, 37, 233, 25, 434, 1]
+console.log(lodash.shuffle(arr));
